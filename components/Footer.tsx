f2f45b4353b79fd8652ae1daa974d6ccfa23887e
unknown <umair.hussain.3947@gmail.com>
2023-07-08 18:38:38 +0530

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,91 @@
+import { footerLinks } from '@/constants.ts'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+type ColumnProps = {
+  title: string,
+  Links: Array<string>
+}
+
+const FooterColumn = ({ title, Links }: ColumnProps) => (
+  <div className='footer_column'>
+    <h4 className='font-semibold'>
+      {title}
+    </h4>
+    <ul className='flex flex-col gap-2 font-normal'>
+      {Links.map((link) =>
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      )}
+    </ul>
+  </div>
+)
+
+const Footer = () => {
+  return (
+    <footer className='flexStart footer'>
+      <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+          <Image
+            src="lgo.svg"
+            width={115}
+            height={38}
+            alt="villanio"
+          />
+          <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+            Uniquely known brand name since 2016.
+          </p>
+
+        </div>
+        <div className='flex flex-wrap gap-12'>
+          <FooterColumn
+            title={footerLinks[0].title}
+            Links={footerLinks[0].links}
+          />
+
+          <div className='flex-1 flex flex-col gap-4'>
+            <FooterColumn
+              title={footerLinks[1].title}
+              Links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              Links={footerLinks[2].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[3].title}
+            Links={footerLinks[3].links}
+          />
+
+          <div className='flex-1 flex flex-col gap-4'>
+            <FooterColumn
+              title={footerLinks[4].title}
+              Links={footerLinks[4].links}
+            />
+            <FooterColumn
+              title={footerLinks[5].title}
+              Links={footerLinks[5].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[6].title}
+            Links={footerLinks[6].links}
+          />
+        </div>
+      </div>
+
+
+      <div className='flexBetween footer_copyright'>
+        <p>@ 2023 Villanio. All rights reserved</p>
+        <p className='text-gray'>
+          <span className='text-black font-semibold'>12,345</span> projects submitted
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer 
